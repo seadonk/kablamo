51102cf08db0def7618bf70d7a2af28d8f53a80d
brian.sebben <brian.sebben@gmail.com>
2022-04-02 12:05:44 -0500

    Updated serpinskiDots

--- libs/drawing/src/lib/canvas/patterns/serpinskiDots.ts
+++ libs/drawing/src/lib/canvas/patterns/serpinskiDots.ts
@@ -3,6 +3,10 @@ import {ArtPatternFn, Coord, draw, Preset} from "@kablamo/drawing";
 export const serpinskiDots: ArtPatternFn = (ctx: CanvasRenderingContext2D, preset: Preset) => {
     const {iterations = 100, scale = 1} = preset;
 
+    const getMidPoint = (a: Coord, b: Coord): Coord => [(a[0] + b[0]) / 2, (a[1] + b[1]) / 2];
+    const drawPoint = ([x,y]: Coord) => ctx.fillRect(x,y,1,1);
+    const getRandVertex = (vertices: Coord[]): Coord => vertices[Math.floor(Math.random() * 3)];
+    const drawVertices = (vertices: Coord[]) => vertices.forEach(t => drawPoint(t));
     const getVertices = (scale: number, ctx: CanvasRenderingContext2D) => {
         const padding = 20;
         const [width, height] = [ctx.canvas.width,ctx.canvas.height];
@@ -12,17 +16,12 @@ export const serpinskiDots: ArtPatternFn = (ctx: CanvasRenderingContext2D, prese
         return [a,b,c].map(t => t.map(c => c*scale) as Coord);
     }
 
-    const vertices: Coord[] = getVertices(scale, ctx);
     draw(ctx, () => {
-        const getRandVertex = (): Coord => vertices[Math.floor(Math.random() * 3)];
-        const getMidPoint = (a: Coord, b: Coord): Coord => [(a[0] + b[0]) / 2, (a[1] + b[1]) / 2];
-        const drawPoint = ([x,y]: Coord) => ctx.fillRect(x,y,1,1);
-        const drawVertices = () => vertices.forEach(t => drawPoint(t));
-
+        const vertices: Coord[] = getVertices(scale, ctx);
         let position: Coord = vertices[0];
-        drawVertices();
+        drawVertices(vertices);
         for (let i = 0; i < iterations; i++) {
-            const vertex = getRandVertex();
+            const vertex = getRandVertex(vertices);
             position = getMidPoint(position, vertex);
             drawPoint(position);
         }
